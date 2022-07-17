d52933be26f737b32be5c1ec6fdb45bb00a432d6
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-07-17 10:32:25 +0530

    Create Q5.c

---
+++ Q5.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main() {
+    for(int i = 10;i>=1;i--)
+    {
+        if(i%2!=0)
+        {
+            printf("\n%d",i);
+        }
+        
+    }
+  
+    return 0;
+}
