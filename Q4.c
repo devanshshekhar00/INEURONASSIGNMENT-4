5466c08ba1f2bc847dfe98b2fc81abb7cedbc80c
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-07-17 10:31:01 +0530

    Create Q4.c

---
+++ Q4.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main() {
+    for(int i = 1;i<=10;i++)
+    {
+        if(i%2!=0)
+        {
+            printf("\n%d",i);
+        }
+        
+    }
+  
+    return 0;
+}
